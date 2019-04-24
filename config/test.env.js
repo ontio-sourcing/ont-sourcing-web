27c372546c9f33f758f03b21647227cf874bac31
whjyoung <1326587275@qq.com>
2019-04-24 16:05:18 +0800

    '修改页面'

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,6 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  API_ROOT:'"http://172.168.3.17:7088/"'
 })
