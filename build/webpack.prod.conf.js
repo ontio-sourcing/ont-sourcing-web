a4f5c73890707e8117ae9f7e139a19ac8fa616e6
MickeyWang <1244134672@qq.com>
2019-04-24 16:27:00 +0800

    add build config

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -11,9 +11,18 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const env = process.env.NODE_ENV === 'testing'
-  ? require('../config/test.env')
-  : require('../config/prod.env')
+let env;
+switch (process.env.NODE_ENV) {
+  case 'production':
+    env = require('../config/prod.env');
+    break;
+  case 'test':
+    env = require('../config/test.env');
+    break;
+
+  default:
+    break;
+}
 
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
