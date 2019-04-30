2e4575b388704b42eba34eb5653834912115fd14
whjyoung <1326587275@qq.com>
2019-04-30 10:51:48 +0800

    'update'

--- config/test.env.js
+++ config/test.env.js
@@ -4,6 +4,6 @@ const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"test"',
-  API_ROOT:'"http://172.168.3.17:7088/"',
+  API_ROOT:'"http://172.168.3.68:7088/"',
   fileUrl:'"/static/ontsourcing%E7%82%B9%E6%99%B4%E6%A8%A1%E6%9D%BF0426.xlsx"'
 })
