e94b290ee0bf7778f2048c22579cdc7366896f7f
ericyang <ericyangchen@foxmail.com>
2019-05-08 18:16:24 +0800

    change prod.env

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   API_ROOT:'"https://sourcing.ont.io:7088/"',
   fileUrl:'"/static/ontsourcing%E7%82%B9%E6%99%B4%E6%A8%A1%E6%9D%BF0426.xlsx"',
-  ONTID_SIGININ: '"http://signin.ontid.pro?params="',
+  ONTID_SIGININ: '"https://signin.ont.io?params="',
   APP_ONTID: '"did:ont:APeE1piJrhsETgpngHsT3hmuvrjAiDQt4N"'
 }
