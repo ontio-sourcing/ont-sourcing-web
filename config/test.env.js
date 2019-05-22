81440275a7a8489c132e29ff3e50f6435b481d8c
ericyang <ericyangchen@foxmail.com>
2019-05-22 17:10:22 +0800

    get url

--- config/test.env.js
+++ config/test.env.js
@@ -6,5 +6,6 @@ module.exports = merge(devEnv, {
   NODE_ENV: '"test"',
   API_ROOT:'"http://172.168.3.68:7088/"',
   fileUrl:'"/static/ontsourcing%E7%82%B9%E6%99%B4%E6%A8%A1%E6%9D%BF0426.xlsx"',
-  ONTID_SIGININ: '"http://139.219.136.188:10390?params="'
+  ONTID_SIGININ: '"http://139.219.136.188:10390?params="',
+  TOC_API_ROOT: '"http://172.168.3.68:9088/"'
 })
